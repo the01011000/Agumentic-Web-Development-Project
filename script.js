96bbd1c94388830f81ad405f86d9c393aba27617
Jay Vasani <96509726+the01011000@users.noreply.github.com>
2023-08-06 19:54:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
 $(document).ready(function() {
-  // Fetch dynamic content from the backend
-  $.ajax({
-    url: "get_hero_content.php",
-    type: "GET",
-    success: function(data) {
-      // Update hero banner content with data received
-      $("#hero-title").text(data.title);
-      $("#hero-description").text(data.description);
-    },
-    error: function() {
-      // Handle error if fetching content fails
-      console.error("Failed to fetch dynamic content.");
-    }
+  // Update Hero Banner Form Submission
+  $("#update-hero-form").submit(function(event) {
+    event.preventDefault();
+
+    var title = $("input[name='title']").val();
+    var description = $("textarea[name='description']").val();
+
+    $.ajax({
+      type: "POST",
+      url: "backend.php",
+      data: {
+        action: "update_hero",
+        title: title,
+        description: description
+      },
+      dataType: "json",
+      success: function(response) {
+        alert(response.message); // Display success message
+      },
+      error: function(error) {
+        console.log(error);
+        alert("Failed to update hero banner. Please try again.");
+      }
+    });
+  });
+
+  // Update Contact Details Form Submission
+  $("#update-contact-form").submit(function(event) {
+    event.preventDefault();
+
+    var address = $("input[name='address']").val();
+    var email = $("input[name='email']").val();
+
+    $.ajax({
+      type: "POST",
+      url: "backend.php",
+      data: {
+        action: "update_contact",
+        address: address,
+        email: email
+      },
+      dataType: "json",
+      success: function(response) {
+        alert(response.message); // Display success message
+      },
+      error: function(error) {
+        console.log(error);
+        alert("Failed to update contact details. Please try again.");
+      }
+    });
   });
 });
